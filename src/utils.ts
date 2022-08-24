170f4c11d3d4ed6b0e11fb8acff07d7aacb3710e
Gabin Desserprit <desserprit.gabin@gmail.com>
2022-08-24 22:12:59 +0200

    Fix missing path resolve

--- src/utils.ts
+++ src/utils.ts
@@ -4,7 +4,7 @@ import path from 'path';
 export const traverseDirectoryUp = (dir: string, file: string): string => {
   const files = fs.readdirSync(dir);
   const parentDir = path.join(dir, '..');
-  if (files.includes(file)) return dir;
+  if (files.includes(file)) return path.resolve(dir, file);
   else if (parentDir !== dir) return traverseDirectoryUp(parentDir, file);
   else return undefined;
 }
